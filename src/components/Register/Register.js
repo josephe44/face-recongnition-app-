a67305ae35c1570cab11f6cc6d650b6771b846dd
josephe44 <josephe442@gmail.com>
2021-02-12 16:49:56 +0100

    update

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -20,7 +20,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignin = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://fast-plateau-03887.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
